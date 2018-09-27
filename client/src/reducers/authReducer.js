5bd975ee82fc1159d28896ec78478bfa9e5de80f
simonmcho <simonmcho@gmail.com>
2018-09-26 21:21:15 -0700

    Boilerplate setup for authreducer and store

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    isAuthenticated: false,
+    user: {}
+    // Can add any key value pair, this will show up in your auth state object
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,20 @@
-import { createStore, applyMiddleware } from 'redux'; 
+import { createStore, applyMiddleware, compose } from 'redux'; 
 import thunk from 'redux-thunk';
+import rootReducer from './reducers'; // import combined reducer
+
+const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(() => [], {}, applyMiddleware(...middleware)); // spread operator allows us to bring any middleware in the variable declartion
+const store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // Allows redux devtools chrome extension
+    )
+);
+//const store = createStore(rootReducer, initialState, applyMiddleware(...middleware)); // spread operator allows us to bring any middleware in the variable declartion
+//const store = createStore(() => [], {}, applyMiddleware(...middleware));
 
 export default store;
