61714bf127ba2b699af9441317c463aa8cb8427d
simonmcho <simonmcho@gmail.com>
2018-09-26 20:01:21 -0700

    Added themes, and then added Login component with basic component functionality

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,76 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      errors: {}  
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const { email, password } = this.state;
+
+    const user = {
+      email,
+      password
+    }
+
+    // Register user
+    console.log(user);
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
-      <div>
-          <h1>LOGIN</h1>
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">Sign in to your Nouvly account</p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input 
+                    type="email" 
+                    className="form-control form-control-lg" 
+                    placeholder="Email Address" 
+                    name="email" 
+                    value={email}
+                    onChange={this.onChange}
+                    />
+                </div>
+                <div className="form-group">
+                  <input 
+                    type="password" 
+                    className="form-control form-control-lg" 
+                    placeholder="Password" 
+                    name="password" 
+                    value={password}
+                    onChange={this.onChange}
+                    />
+                </div>
+                <input 
+                  type="submit" 
+                  className="btn btn-info btn-block mt-4" 
+                />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
